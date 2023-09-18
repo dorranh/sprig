e9c6ed2392f2d656f7c864ab4e481b1c615af32f
Dorran Howell <dorranh@appalachian.dev>
2023-09-18 11:45:20 +0200

    Update Nix flake
    Added alejandra as a formatter for Nix files and applied formatting.

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
-  description = "A basic flake with a shell";
+  description = "The Nix flake for Sprig. Currently this only provides a dev shell.";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = pkgs.mkShell {
-        packages = [ pkgs.sbt pkgs.openjdk_headless pkgs.zlib pkgs.graalvm11];
+        packages = [pkgs.sbt pkgs.alejandra pkgs.openjdk_headless pkgs.zlib pkgs.graalvm11 pkgs.coursier];
       };
     });
 }
