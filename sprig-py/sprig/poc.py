76e482fdfd8aff0a00a5487e62a3834556aa43b7
Dorran Howell <dorranh@appalachian.dev>
2024-03-19 11:40:44 +0100

    [WIP] Hacking on PoC

--- sprig-py/sprig/poc.py
+++ sprig-py/sprig/poc.py
@@ -32,12 +32,33 @@ class LocalStorage(Storage):
         return open(self.path)
 
 
+class RowConfig(BaseModel):
+    start: int
+    stop: int | None
+
+
 class Sprig(BaseModel):
     _id: UUID4
     name: str
     storage: Storage
+    structure: Structure
+    read_config: RowConfig  # FIXME
 
 
+# TODO: Decorator for grabbing metadata from read call
+# TODO: Add support for tracking callsite
+def tracked(func):
+    def wrapper(*args, **kwargs):
+        # FIXME: do something less silly here
+        sprig: Sprig = args[0]
+        with open(f"./sprigs/{sprig.name}.sprig", "w") as f:
+            print(sprig.model_dump_json(), file=f)
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+@tracked
 def read_rows(sprig: Sprig, start: int = 0, stop: int | None = None):
     stream = sprig.storage.open()
     # FIXME - Just for prototyping I am loading everything into memory
@@ -54,14 +75,24 @@ def read_rows(sprig: Sprig, start: int = 0, stop: int | None = None):
     return rows
 
 
+def read(sprig: Sprig):
+    match sprig.structure:
+        case Structure.ROWS:
+            read_rows(sprig, start=sprig.read_config.start, stop=sprig.read_config.stop)
+        case _:
+            raise RuntimeError("Explode!")
+
+
 def main():
     sprig = Sprig(
         _id=uuid4(),
         name="demo-sprig",
+        structure=Structure.ROWS,
+        read_config=RowConfig(start=0, stop=None),
         storage=LocalStorage(path="./example-data/my-dataset.csv"),
     )
     print(f"Here is your sprig: {sprig}")
-    print(read_rows(sprig))
+    print(read(sprig))
 
 
 if __name__ == "__main__":
