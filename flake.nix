cc451f96e02cc138ab2097696931aacce71627ed
Dorran Howell <dorranh@appalachian.dev>
2024-03-30 10:39:06 +0100

    Try out using just + flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "The Nix flake for Sprig. Currently this only provides a dev shell.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.just
+          pkgs.python3
+          pkgs.poetry
+          pkgs.alejandra
+        ];
+
+        shellHook = ''
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib"
+        '';
+      };
+    });
+}
